b3a196b6bbed36183bd30dd5f07378ae3e60dd5b
Chris Kim <ckim990@west-mec.org>
2019-04-28 21:27:59 -0700

    JS and added links

--- mas_reg2.js
+++ mas_reg2.js
@@ -20,4 +20,15 @@
 
 
 */
+window.addEventListener("load", writeSessionValues);
 
+function writeSessionValues() {
+      document.getElementById("regName").textContent = sessionStorage.getItem("confName");
+      document.getElementById("regGroup").textContent = sessionStorage.getItem("confGroup");
+      document.getElementById("regEmail").textContent = sessionStorage.getItem("confMail");
+      document.getElementById("regPhone").textContent = sessionStorage.getItem("confPhone");
+      document.getElementById("regSession").textContent = sessionStorage.getItem("confSession");
+      document.getElementById("regBanquet").textContent = sessionStorage.getItem("confBanquet");
+      document.getElementById("regPack").textContent = sessionStorage.getItem("confPack");
+      document.getElementById("regTotal").textContent = "$" + sessionStorage.getItem("confTotal");
+}
