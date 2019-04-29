f20abfbdffa7615e799574775aceb2fdbd989427
Chris Kim <ckim990@west-mec.org>
2019-04-28 21:29:26 -0700

    Final Commit

--- mas_register.js
+++ mas_register.js
@@ -27,4 +27,69 @@
 
 
 */
+window.addEventListener("load", function () {
+   calcCart()
+   var regForm = document.forms.regForm;
+   regForm.elements.regSubmit.onclick = sessionsTest;
+   regForm.elements.fnBox.onblur = calcCart;
+   regForm.elements.lnBox.onblur = calcCart;
+   regForm.elements.groupBox.onblur = calcCart;
+   regForm.elements.mailBox.onblur = calcCart;
+   regForm.elements.phoneBox.onblur = calcCart;
+   regForm.elements.banquetBox.onblur = calcCart;
+   regForm.elements.sessionBox.onchange = calcCart;
+   regForm.elements.mediaCB.onclick = calcCart;
+})
 
+function sessionsTest() {
+   var sessionBox = document.getElementById("sessionBox");
+   if (sessionBox.selectedIndex === -1) {
+         sessionBox.setCustomValidity("Select a Session Package")
+   } else {
+         sessionBox.setCustomValidity("");
+   }
+};
+
+function calcCart() {
+   sessionStorage.setItem("confName", document.getElementById("fnBox").value + " " + document.getElementById("lnBox").value + "")
+   sessionStorage.setItem("confGroup", document.getElementById("groupBox").value)
+   sessionStorage.setItem("confMail", document.getElementById("mailBox").value)
+   sessionStorage.setItem("confPhone", document.getElementById("phoneBox").value)
+   sessionStorage.setItem("confBanquet", document.getElementById("banquetBox").value)
+   var confBanquet = document.getElementById("banquetBox").value;
+   sessionStorage.setItem("confBanquetCost", confBanquet * 55)
+   var confBanquetCost = confBanquet * 55;
+   var sessionBox = document.getElementById("sessionBox");
+
+   if (sessionBox.selectedIndex !== -1) {
+         sessionStorage.setItem("confSession", sessionBox[sessionBox.selectedIndex].innerText)
+         sessionStorage.setItem("confSessionCost", sessionBox[sessionBox.selectedIndex].value)
+         var confSessionCost = sessionBox[sessionBox.selectedIndex].value;
+   } else {
+         sessionStorage.setItem("confSession", "")
+         sessionStorage.setItem("confSessionCost", 0)
+         var confSessionCost = 0;
+   }
+   if (document.getElementById("mediaCB").checked == true) {
+         sessionStorage.setItem("confPack", "yes")
+         sessionStorage.setItem("confPackCost", 115)
+         var confPackCost = 115;
+   } else {
+         sessionStorage.setItem("confPack", "no")
+         sessionStorage.setItem("confPackCost", 0)
+         var confPackCost = 0;
+   }
+   sessionStorage.setItem("confTotal", parseFloat(confSessionCost) + parseFloat(confBanquetCost) + parseFloat(confPackCost));
+   writeSessionValues();
+};
+
+function writeSessionValues() {
+   document.getElementById("regName").textContent = sessionStorage.getItem("confName");
+   document.getElementById("regGroup").textContent = sessionStorage.getItem("confGroup");
+   document.getElementById("regEmail").textContent = sessionStorage.getItem("confMail");
+   document.getElementById("regPhone").textContent = sessionStorage.getItem("confPhone");
+   document.getElementById("regSession").textContent = sessionStorage.getItem("confSession");
+   document.getElementById("regBanquet").textContent = sessionStorage.getItem("confBanquet");
+   document.getElementById("regPack").textContent = sessionStorage.getItem("confPack");
+   document.getElementById("regTotal").textContent = "$" + sessionStorage.getItem("confTotal")
+}
